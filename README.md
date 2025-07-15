335507b758fbf3e15c7f1825127c774acb680712
pakiZBRG <pavlovicnikola511@gmail.com>
2025-07-15 11:14:19 +0200

    Adding error handling middleware and app middleware

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple backend API application for creating, tracking and renewing subscription for individual users. Created using mongoDB, express, Node.js
--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-import express from 'express';
+import express from "express";
+import cookieParser from "cookie-parser";
 
-import { PORT } from './config/env.js'
-
-import userRouter from './routes/user.routes.js'
-import authRouter from './routes/auth.routes.js'
-import subscriptionRouter from './routes/subscription.routes.js'
-import connectToDB from './database/mongodb.js';
+import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDB from "./database/mongodb.js";
+import errorMiddleware from "./middleware/errorMiddleware.js";
 
 const app = express();
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send("Cao")
-})
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
+
+app.get("/", (req, res) => {
+  res.send("Cao");
+});
 
 app.listen(PORT, async () => {
   console.log(`Listening on port ${PORT}`);
 
   await connectToDB();
-})
+});
 
 export default app;
